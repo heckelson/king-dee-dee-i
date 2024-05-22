861523f8c81bd828f98c9ca15bd0922dd11294c8
Florian Pollak <bfourdev@gmail.com>
2024-05-22 22:57:10 +0200

    transplant old frontend into quasar frontend

---
+++ frontend2/src/components/RiskScore.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="text-center">
+    <h1 class="h1">Risk Score</h1>
+    <div id="risk-score" class="container container-sm">{{ number }}</div>
+  </div>
+</template>
+
+<script setup>
+const number = 12;
+</script>
+
+<style>
+#risk-score {
+  max-width: 80px;
+  height: 80px;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+
+  color: var("color");
+}
+</style>
--- frontend2/src/pages/IndexPage.vue
+++ frontend2/src/pages/IndexPage.vue
@@ -1,17 +1,56 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="container text-center">
+            <img src="src/assets/dedede.png" alt="" />
+          </div>
+
+          <MedicationPicker class="card" />
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-4">
+          <RiskScore class="card" />
+        </div>
+        <div class="col-sm-8">
+          <KnownInteractions class="card" />
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
+import MedicationPicker from "src/components/MedicationPicker.vue";
+import RiskScore from "src/components/RiskScore.vue";
+import KnownInteractions from "src/components/KnownInteractions.vue";
 
 export default defineComponent({
-  name: 'IndexPage'
+  name: "IndexPage",
+  components: {
+    MedicationPicker,
+    RiskScore,
+    KnownInteractions,
+  },
 });
 </script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.card {
+  margin-top: 12px;
+}
+</style>

c050bdd5c78b6ef9089c16c7f2fc007f82f53381
Florian Pollak <bfourdev@gmail.com>
2024-05-23 16:45:57 +0200

    Revert "Auxiliary commit to revert individual files from f786559ebdb763091af8583611b1b681704598a3"
    This reverts commit 84fcf6ed922ba7f022ea19f3b6cf446f007c8020.

--- frontend/src/components/KnownInteractions.vue
+++ frontend/src/components/KnownInteractions.vue
@@ -1,81 +1,81 @@
 <template>
-    <div class="text-center">
-        <h1>Known Interactions</h1>
-        <div class="container">
-            <p>Table</p>
+  <div class="text-center">
+    <h1>Known Interactions</h1>
+    <div class="container">
+      <p>Table</p>
 
-            <div class="container text-center" v-show="loadingSpinnerShown">
-                <div class="spinner-grow text-primary" role="status">
-                    <span class="visually-hidden">Loading...</span>
-                </div>
-            </div>
-
-            <pre class="text-start">{{ this.interactions }}</pre>
+      <div class="container text-center" v-show="loadingSpinnerShown">
+        <div class="spinner-grow text-primary" role="status">
+          <span class="visually-hidden">Loading...</span>
         </div>
+      </div>
+
+      <pre class="text-start">{{ this.interactions }}</pre>
     </div>
+  </div>
 </template>
 
 <script>
-import { useMedStore } from 'stores/store';
-import { mapStores, storeToRefs } from 'pinia';
+import { useMedStore } from "@/store";
+import { mapStores, storeToRefs } from "pinia";
 
-import { SERVER_URL } from 'src/constants';
+import { SERVER_URL } from "@/constants";
 
 export default {
-    data() {
-        return {
-            interactions: [],
-            medStore: undefined, // defined in `mounted()`
-            loadingSpinnerShown: false,
-        };
-    },
+  data() {
+    return {
+      interactions: [],
+      medStore: undefined, // defined in `mounted()`
+      loadingSpinnerShown: false,
+    };
+  },
 
-    mounted() {
-        this.medStore = storeToRefs(this.medStoreStore);
-    },
+  mounted() {
+    this.medStore = storeToRefs(this.medStoreStore);
+  },
 
-    methods: {
-        fetchInteractions() {
-            const encodedSelection = encodeURI(this.medStore.selectedMeds);
+  methods: {
+    fetchInteractions() {
+      const encodedSelection = encodeURI(this.medStore.selectedMeds);
 
-            this.loadingSpinnerShown = true;
+      this.loadingSpinnerShown = true;
 
-            if (this.medStore.selectedMeds.length >= 2) {
-                fetch(
-                    `${SERVER_URL}/mock-interactions?selectedMeds=${encodedSelection}`
-                )
-                    .then((resp) => {
-                        resp.json().then((body) => {
-                            this.interactions = body;
-                        });
-                    })
-                    .catch((err) => {
-                        console.error(err);
-                    })
-                    .finally(() => {
-                        this.loadingSpinnerShown = false;
-                    });
-            }
-        },
+      if (this.medStore.selectedMeds.length >= 2) {
+        fetch(
+          `${SERVER_URL}/mock-interactions?selectedMeds=${encodedSelection}`,
+        )
+          .then((resp) => {
+            resp.json().then((body) => {
+              this.interactions = body;
+            });
+          })
+          .catch((err) => {
+            console.error(err);
+          })
+          .finally(() => {
+            this.loadingSpinnerShown = false;
+          });
+      }
     },
-    computed: {
-        ...mapStores(useMedStore),
-    },
+  },
+  computed: {
+    ...mapStores(useMedStore),
+  },
 
-    watch: {
-        medStore: {
-            handler(newState, _) {
-                if (newState === undefined) return;
-                if (newState.selectedMeds.length > 1) {
-                    this.fetchInteractions();
-                } else {
-                    // we can't have interactions with only 1 med, thus we clear the variable.
-                    this.interactions = [];
-                }
-            },
-            deep: true,
-        },
+  watch: {
+    medStore: {
+      handler(newState, _) {
+        if (newState === undefined) return;
+        if (newState.selectedMeds.length > 1) {
+          this.fetchInteractions();
+        } else {
+          // we can't have interactions with only 1 med, thus we clear the variable.
+          this.interactions = [];
+        }
+      },
+      deep: true,
     },
+  },
 };
 </script>
 
--- frontend/src/components/MedicationPicker.vue
+++ frontend/src/components/MedicationPicker.vue
@@ -1,190 +1,160 @@
 <template>
-    <div class="container text-center">
-        <div class="col">
-            <h1 class="h1">Medication Picker</h1>
+  <div class="container text-center">
+    <div class="col">
+      <h1 class="h1">Medication Picker</h1>
 
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-5">
-                        <h3>Add Medication</h3>
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-5">
+            <h3>Add Medication</h3>
 
-                        <form
-                            @submit.prevent="searchForMedication()"
-                            class="input-group"
-                        >
-                            <input
-                                class="input-group-text"
-                                v-model="searchString"
-                                placeholder="Search medication"
-                            />
-                            <button class="btn btn-primary">Search</button>
-                        </form>
+            <form @submit.prevent="searchForMedication()" class="input-group">
+              <input
+                class="input-group-text"
+                v-model="searchString"
+                placeholder="Search medication"
+              />
+              <button class="btn btn-primary">Search</button>
+            </form>
 
-                        <div class="container card">
-                            <h4>Results</h4>
+            <div class="container card">
+              <h4>Results</h4>
 
-                            <div
-                                class="container text-center"
-                                v-show="loadingSpinnerShown"
-                            >
-                                <div
-                                    class="spinner-grow text-primary"
-                                    role="status"
-                                >
-                                    <span class="visually-hidden"
-                                        >Loading...</span
-                                    >
-                                </div>
-                            </div>
-
-                            <div id="search-results">
-                                <table class="table table-hover">
-                                    <tbody>
-                                        <tr
-                                            v-for="result of searchResults"
-                                            v-bind:key="result"
-                                        >
-                                            <td class="text-start">
-                                                {{ result }}
-                                            </td>
-                                            <td>
-                                                <button
-                                                    class="btn btn-sm btn-primary"
-                                                    @click="
-                                                        this.addMedicationToSelection(
-                                                            result
-                                                        )
-                                                    "
-                                                >
-                                                    Add
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-sm-7">
-                        <h3>Selected Medications</h3>
-
-                        <div>
-                            <button
-                                class="btn btn-danger"
-                                @click="this.clearMedicationSelection()"
-                            >
-                                Clear All
-                            </button>
-                        </div>
-                        <div>
-                            <table class="table table-hover">
-                                <tbody>
-                                    <tr
-                                        v-for="medication of this.medStoreStore
-                                            .selectedMeds"
-                                        v-bind:key="medication"
-                                    >
-                                        <td class="text-start">
-                                            {{ medication }}
-                                        </td>
-                                        <td>
-                                            <button
-                                                class="btn btn-sm btn-danger"
-                                                @click="
-                                                    this.removeMedicationFromSelection(
-                                                        medication
-                                                    )
-                                                "
-                                            >
-                                                Remove
-                                            </button>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
+              <div class="container text-center" v-show="loadingSpinnerShown">
+                <div class="spinner-grow text-primary" role="status">
+                  <span class="visually-hidden">Loading...</span>
                 </div>
+              </div>
+
+              <div id="search-results">
+                <table class="table table-hover">
+                  <tbody>
+                    <tr v-for="result of searchResults" v-bind:key="result">
+                      <td class="text-start">{{ result }}</td>
+                      <td>
+                        <button
+                          class="btn btn-sm btn-primary"
+                          @click="this.addMedicationToSelection(result)"
+                        >
+                          Add
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
+          </div>
+
+          <div class="col-sm-7">
+            <h3>Selected Medications</h3>
+
+            <div>
+              <button
+                class="btn btn-danger"
+                @click="this.clearMedicationSelection()"
+              >
+                Clear All
+              </button>
+            </div>
+            <div>
+              <table class="table table-hover">
+                <tbody>
+                  <tr
+                    v-for="medication of this.medStoreStore.selectedMeds"
+                    v-bind:key="medication"
+                  >
+                    <td class="text-start">{{ medication }}</td>
+                    <td>
+                      <button
+                        class="btn btn-sm btn-danger"
+                        @click="this.removeMedicationFromSelection(medication)"
+                      >
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-import { useMedStore } from 'stores/store';
-import { mapStores, storeToRefs } from 'pinia';
+import { useMedStore } from "@/store";
+import { mapStores, storeToRefs } from "pinia";
 
-import { SERVER_URL } from 'src/constants';
+import { SERVER_URL } from "@/constants";
 
 export default {
-    data() {
-        return {
-            searchString: '',
-            searchResults: [],
-            medStore: undefined,
-            loadingSpinnerShown: false,
-        };
+  data() {
+    return {
+      searchString: "",
+      searchResults: [],
+      medStore: undefined,
+      loadingSpinnerShown: false,
+    };
+  },
+
+  mounted() {
+    this.medStore = storeToRefs(this.medStoreStore);
+  },
+
+  methods: {
+    searchForMedication() {
+      if (this.searchString.length > 2) {
+        this.searchResults = [];
+        this.loadingSpinnerShown = true;
+
+        fetch(`${SERVER_URL}/mock-search/${this.searchString}`)
+          .then((resp) => {
+            resp.json().then((body) => {
+              this.searchResults = body["searchResults"];
+
+              // filter out all elements that are already in our selection.
+              this.searchResults = this.searchResults.filter(
+                (elem) => this.medStoreStore.selectedMeds.indexOf(elem) === -1,
+              );
+            });
+          })
+          .catch((err) => {
+            // TODO: Handle this better.
+            console.error(err);
+          })
+          .finally(() => {
+            this.loadingSpinnerShown = false;
+          });
+      }
     },
 
-    mounted() {
-        this.medStore = storeToRefs(this.medStoreStore);
+    addMedicationToSelection(medication) {
+      this.medStoreStore.addMedication(medication);
+
+      // reset the form.
+      this.$data.searchResults = [];
+      this.searchString = "";
     },
 
-    methods: {
-        searchForMedication() {
-            if (this.searchString.length > 2) {
-                this.searchResults = [];
-                this.loadingSpinnerShown = true;
-
-                fetch(`${SERVER_URL}/mock-search/${this.searchString}`)
-                    .then((resp) => {
-                        resp.json().then((body) => {
-                            this.searchResults = body['searchResults'];
-
-                            // filter out all elements that are already in our selection.
-                            this.searchResults = this.searchResults.filter(
-                                (elem) =>
-                                    this.medStoreStore.selectedMeds.indexOf(
-                                        elem
-                                    ) === -1
-                            );
-                        });
-                    })
-                    .catch((err) => {
-                        // TODO: Handle this better.
-                        console.error(err);
-                    })
-                    .finally(() => {
-                        this.loadingSpinnerShown = false;
-                    });
-            }
-        },
-
-        addMedicationToSelection(medication) {
-            this.medStoreStore.addMedication(medication);
-
-            // reset the form.
-            this.$data.searchResults = [];
-            this.searchString = '';
-        },
-
-        removeMedicationFromSelection(medication) {
-            this.medStoreStore.removeMedication(medication);
-        },
-
-        clearMedicationSelection() {
-            this.medStoreStore.clearAllSelectedMeds();
-        },
+    removeMedicationFromSelection(medication) {
+      this.medStoreStore.removeMedication(medication);
     },
-    computed: {
-        ...mapStores(useMedStore),
+
+    clearMedicationSelection() {
+      this.medStoreStore.clearAllSelectedMeds();
     },
+  },
+  computed: {
+    ...mapStores(useMedStore),
+  },
 };
 </script>
 
 <style scoped>
 #search-results {
-    min-height: 200px;
+  min-height: 200px;
 }
 </style>

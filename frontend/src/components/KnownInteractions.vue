5d76039b3ddfdb4530a830ce0dee13341bdc4cc0
Florian Pollak <bfourdev@gmail.com>
2024-05-24 01:40:11 +0200

    show grouped interaction results

--- frontend/src/components/KnownInteractions.vue
+++ frontend/src/components/KnownInteractions.vue
@@ -1,30 +1,55 @@
 <template>
   <div class="column items-center">
-    <div class="q-pa-md" style="max-width: 700px">
-      <q-expansion-item v-model="isAccordionExpanded" class="shadow-1 overflow-hidden" style="border-radius: 24px"
-        :icon="icon" :label="label" :header-class="`bg-${color} text-white`" :expand-icon-class="isLoading ||
-        interactions.searchResults == null ||
-        interactions.searchResults?.length === 0
-        ? 'hidden'
-        : 'text-white'
-        ">
-        <q-card>
-          <q-card-section>
-            <div class="row">
-              <q-list>
-                <div v-for="interaction in interactions.searchResults" v-bind:key="interaction">
-                  <q-item>
-                    <q-item-section>
-                      <q-item-label class="text-h6">
-                        {{ interaction.condition_concept_name }}</q-item-label>
-                    </q-item-section>
-                  </q-item>
+    <div class="q-pa-md" style="max-width: 500px">
+      <div v-if="isLoading || (interactions.searchResults?.length ?? 0) === 0">
+        <q-expansion-item
+          class="shadow-1 overflow-hidden"
+          style="border-radius: 24px"
+          :icon="icon"
+          :label="label"
+          :header-class="`bg-${color} text-white`"
+          expand-icon-class="hidden"
+        >
+        </q-expansion-item>
+      </div>
+
+      <div v-else>
+        <div
+          v-for="medicationPair in Object.keys(groupedInteractions)"
+          v-bind:key="medicationPair"
+          class="q-mb-md"
+        >
+          <q-expansion-item
+            class="shadow-1 overflow-hidden"
+            style="border-radius: 24px"
+            icon="warning"
+            :label="`${groupedInteractions[medicationPair].length} possible interactions between ${medicationPair}`"
+            :header-class="`bg-deep-orange text-white`"
+            expand-icon-class="text-white"
+          >
+            <q-card>
+              <q-card-section>
+                <div class="row">
+                  <q-list>
+                    <div
+                      v-for="interaction in groupedInteractions[medicationPair]"
+                      v-bind:key="interaction"
+                    >
+                      <q-item>
+                        <q-item-section>
+                          <q-item-label class="text-h6">
+                            {{ interaction }}</q-item-label
+                          >
+                        </q-item-section>
+                      </q-item>
+                    </div>
+                  </q-list>
                 </div>
-              </q-list>
-            </div>
-          </q-card-section>
-        </q-card>
-      </q-expansion-item>
+              </q-card-section>
+            </q-card>
+          </q-expansion-item>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -37,37 +62,61 @@ const medStore = useMedStore();
 const color = ref("green");
 const icon = ref("check");
 const label = ref("No known interactions");
-const isAccordionExpanded = ref(false);
 
 const isLoading = computed(() => medStore.isLoading);
 const interactions = computed(() => medStore.interactions);
 
+const groupedInteractions = computed(() => {
+  const interactionsList = interactions.value.searchResults;
+  const groupedInteractions = {};
+
+  if (interactionsList) {
+    interactionsList.forEach((interaction) => {
+      const {
+        drug_1_concept_name,
+        drug_2_concept_name,
+        condition_concept_name,
+      } = interaction;
+
+      const key = `${drug_1_concept_name} and ${drug_2_concept_name}`;
+      if (!groupedInteractions[key]) {
+        groupedInteractions[key] = [];
+      }
+
+      groupedInteractions[key].push(condition_concept_name);
+    });
+  }
+
+  console.log("groupedInteractions", groupedInteractions);
+  return groupedInteractions;
+});
+
+watch(isLoading, () => {
+  if (isLoading.value === true) {
+    color.value = "blue";
+    icon.value = "hourglass_empty";
+    label.value = "Looking for interactions ...";
+    return;
+  }
+});
+
 watch(
-  isLoading,
+  interactions,
   () => {
-    if (isLoading.value === true) {
-      color.value = "blue";
-      icon.value = "hourglass_empty";
-      label.value = "Looking for interactions ...";
-      isAccordionExpanded.value = false;
-      return;
-    }
-  }
-);
-
-watch(interactions, () => {
-  if (interactions.value.searchResults?.length > 0) {
-    color.value = "deep-orange";
-    icon.value = "warning";
-    label.value = `${interactions.value?.searchResults?.length ?? 0
+    if (interactions.value.searchResults?.length > 0) {
+      color.value = "deep-orange";
+      icon.value = "warning";
+      label.value = `${
+        interactions.value?.searchResults?.length ?? 0
       } possible interactions are known`;
-  } else {
-    color.value = "green";
-    icon.value = "check";
-    label.value = "No known interactions";
-  }
-}, { deep: true });
-
+    } else {
+      color.value = "green";
+      icon.value = "check";
+      label.value = "No known interactions";
+    }
+  },
+  { deep: true }
+);
 </script>
 
 <style></style>

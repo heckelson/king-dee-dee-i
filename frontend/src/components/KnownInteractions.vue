dd8488939329aeaf161c2931d0ba79fe80ed5cd5
Florian Pollak <bfourdev@gmail.com>
2024-05-24 00:33:32 +0200

    convert KnownInteractions to setup script

--- frontend/src/components/KnownInteractions.vue
+++ frontend/src/components/KnownInteractions.vue
@@ -1,34 +1,22 @@
 <template>
   <div class="column items-center">
     <div class="q-pa-md" style="max-width: 700px">
-      <q-expansion-item
-        v-model="isAccordionExpanded"
-        class="shadow-1 overflow-hidden"
-        style="border-radius: 24px"
-        :icon="icon"
-        :label="label"
-        :header-class="`bg-${color} text-white`"
-        :expand-icon-class="
-          isLoading ||
-          interactions.searchResults == null ||
-          interactions.searchResults?.length === 0
-            ? 'hidden'
-            : 'text-white'
-        "
-      >
+      <q-expansion-item v-model="isAccordionExpanded" class="shadow-1 overflow-hidden" style="border-radius: 24px"
+        :icon="icon" :label="label" :header-class="`bg-${color} text-white`" :expand-icon-class="isLoading ||
+        interactions.searchResults == null ||
+        interactions.searchResults?.length === 0
+        ? 'hidden'
+        : 'text-white'
+        ">
         <q-card>
           <q-card-section>
             <div class="row">
               <q-list>
-                <div
-                  v-for="interaction in this.interactions.searchResults"
-                  v-bind:key="interaction"
-                >
+                <div v-for="interaction in interactions.searchResults" v-bind:key="interaction">
                   <q-item>
                     <q-item-section>
                       <q-item-label class="text-h6">
-                        {{ interaction.condition_concept_name }}</q-item-label
-                      >
+                        {{ interaction.condition_concept_name }}</q-item-label>
                     </q-item-section>
                   </q-item>
                 </div>
@@ -41,81 +29,53 @@
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, watch } from "vue";
 import { useMedStore } from "stores/store";
-import { mapStores, storeToRefs } from "pinia";
 
-import { SERVER_URL } from "src/constants";
+const medStore = useMedStore();
+const interactions = medStore.interactions;
+const color = ref("green");
+const icon = ref("check");
+const label = ref("No known interactions");
+const isAccordionExpanded = ref(false);
 
-export default {
-  data() {
-    return {
-      interactions: {},
-      medStore: undefined, // defined in `mounted()`
-      color: "green",
-      icon: "check",
-      label: "No known interactions",
-      isAccordionExpanded: false,
-    };
-  },
 
-  mounted() {
-    this.medStore = storeToRefs(this.medStoreStore);
-  },
-
-  methods: {
-    async fetchInteractions() {
-      this.medStore.fetchInteractions(this.medStore.selectedMeds);
-    },
-  },
-  computed: {
-    ...mapStores(useMedStore),
-    isLoading: () => this.medStore.isLoading,
-  },
-
-  watch: {
-    medStore: {
-      handler(newState, _) {
-        if (newState === undefined) return;
-        if (newState.selectedMeds?.length > 1) {
-          this.fetchInteractions();
-        } else {
-          // we can't have interactions with only 1 med, thus we clear the variable.
-          this.interactions = [];
-        }
-      },
-      deep: true,
-    },
-    medStore: {
-      handler(newState, _) {
-        if (this.isLoading) {
-          this.color = "blue";
-          this.icon = "hourglass_empty";
-          this.label = "Looking for interactions ...";
-          this.isAccordionExpanded = false;
-          return;
-        }
-      },
-    },
-    interactions: {
-      handler(newState, _) {
-        console.log("interactions changed", newState);
-        if (newState.searchResults?.length > 0) {
-          this.color = "deep-orange";
-          this.icon = "warning";
-          this.label = `${
-            this.interactions?.searchResults?.length ?? 0
-          } possible interactions are known`;
-        } else {
-          this.color = "green";
-          this.icon = "check";
-          this.label = "No known interactions";
-        }
-      },
-      deep: true,
-    },
-  },
+const fetchInteractions = () => {
+  this.medStore.fetchInteractions(this.medStore.selectedMeds);
 };
+
+const isLoading = computed(() => medStore.isLoading);
+
+
+watch(
+  medStore,
+  () => {
+    if (isLoading.value) {
+      color.value = "blue";
+      icon.value = "hourglass_empty";
+      label.value = "Looking for interactions ...";
+      isAccordionExpanded.value = false;
+      return;
+    }
+  },
+  { deep: true }
+);
+
+watch(interactions, () => {
+  console.log("interactions changed", interactions);
+  if (interactions.searchResults?.length > 0) {
+    color.value = "deep-orange";
+    icon.value = "warning";
+    label.value = `${interactions?.searchResults?.length ?? 0
+      } possible interactions are known`;
+  } else {
+    color.value = "green";
+    icon.value = "check";
+    label.value = "No known interactions";
+  }
+}, { deep: true });
+
 </script>
 
 <style></style>
